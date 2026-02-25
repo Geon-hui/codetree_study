b087186d3d4bd22116058737bb7969bdd780ed06
GeonHui Kim <84655228+Geon-hui@users.noreply.github.com>
2026-02-25 15:27:40 +0900

    Title: 문자 제거하기, Time: 80ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260225/문자 제거하기/del-char.py
@@ -0,0 +1,12 @@
+arr=input()
+li_arr=list(arr)
+number=[int(input()) for _ in range(len(arr)-1)]
+
+for i in number:
+    if i>len(li_arr):
+        li_arr.pop(-1)
+        print(''.join(li_arr))
+    else:
+        li_arr.pop(i)
+        print(''.join(li_arr))
+
